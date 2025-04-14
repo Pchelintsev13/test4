77543a70ab152ac25b24c898f5ab3cb27dbd26c4
Pchelintsev13 <91620341+Pchelintsev13@users.noreply.github.com>
2025-04-14 10:34:06 +0400

    Add files via upload

--- MDK-ARM/Morse.h
+++ MDK-ARM/Morse.h
@@ -2,18 +2,10 @@
 #include "ctype.h"
 #include "main.h"
 
-
-
-
-void textLedMorse(TIM_HandleTypeDef *htim11,char *text,uint16_t spidMorse);// starts displaying the Morse code sequence on an led using a timer
-
-void prerivTim(); //Handles the timer interrupt and sends the next Morse symbol
-
-textSizeMorse textInMorse(char* text);// converts plain text to Morse code
+void sendMorse(const char *text);
+void sendMorseSymbol(char symbol);
 
 typedef struct {
-    char SIM;            // uppercase letters
-		char sim;            // lowercase letters
-    char *morse;				 // morse code 
-		uint8_t sizMorzeCod; // size morse code 
+    char character;
+    const char *morse;
 } MorseCode;
